0db169730d7358845caec2f2bd578ce55b5603a3
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-04-18 14:48:41 +0530

    created routes for login and register

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/screens/home_screen.dart';
 import 'package:mynotes/screens/login_view.dart';
+import 'package:mynotes/screens/register_view.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -24,6 +25,10 @@ class HomePage extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const HomeScreen(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     );
   }
 }
