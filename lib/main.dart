3b5bf76ae5c0df070dda84956b044a44532fc719
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-04-25 19:36:26 +0530

    added notes route

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/screens/home_screen.dart';
 import 'package:mynotes/screens/login_view.dart';
+import 'package:mynotes/screens/my_notes_view.dart';
 import 'package:mynotes/screens/register_view.dart';
 import 'firebase_options.dart';
 
@@ -28,6 +29,7 @@ class HomePage extends StatelessWidget {
       routes: {
         '/login/': (context) => const LoginView(),
         '/register/': (context) => const RegisterView(),
+        '/notes/': (context) => const NotesView(),
       },
     );
   }
