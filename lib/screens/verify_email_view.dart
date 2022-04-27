79a8ce657d82140f7a9e68b223fe131e6c8a84a6
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-04-27 14:28:47 +0530

    Error Handling in Register View, Next Screen After Registration

--- lib/screens/verify_email_view.dart
+++ lib/screens/verify_email_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({Key? key}) : super(key: key);
@@ -19,13 +20,28 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Center(child: const Text('Please verify your email address')),
+        const Center(
+            child: Text(
+                'we have sent a email verification. Please open it to verify your account')),
+        const Center(
+            child: Text(
+                'If you haven\'t received verification email yet, click the below button to get verification link to your email id')),
         Center(
           child: TextButton(
             onPressed: () {
               sendVerificationLink();
             },
             child: const Text('Click to Send Verification Link'),
+          ),
+        ),
+        Center(
+          child: TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil(registerRoute, (route) => false);
+            },
+            child: const Text('Restart'),
           ),
         )
       ],
