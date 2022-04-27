e6455a4b04be15e94c9460ff8166b73bbc115aaa
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-04-27 14:09:01 +0530

    created verify email routes

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,3 +1,5 @@
 const notesRoute = '/notes/';
 const registerRoute = '/register/';
 const loginRoute = '/login/';
+const verifyEmailRoute = '/verify-email/';
+
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:mynotes/screens/home_screen.dart';
 import 'package:mynotes/screens/login_view.dart';
 import 'package:mynotes/screens/my_notes_view.dart';
 import 'package:mynotes/screens/register_view.dart';
+import 'package:mynotes/screens/verify_email_view.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -31,6 +32,7 @@ class HomePage extends StatelessWidget {
         loginRoute: (context) => const LoginView(),
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
+        verifyEmailRoute: (context) => const VerifyEmailView(),
       },
     );
   }
